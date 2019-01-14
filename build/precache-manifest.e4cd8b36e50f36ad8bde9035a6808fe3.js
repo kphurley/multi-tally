bcc9eee867622184b184cf8d67275496f1b93121
Kevin Hurley <kphurley@gmail.com>
2019-01-13 21:43:40 -0600

    Initial build subtree commit

---
+++ build/precache-manifest.e4cd8b36e50f36ad8bde9035a6808fe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa12e519ac0894fa1039",
+    "url": "/static/js/main.aa12e519.chunk.js"
+  },
+  {
+    "revision": "d845a7b435e466218615",
+    "url": "/static/js/1.d845a7b4.chunk.js"
+  },
+  {
+    "revision": "aa12e519ac0894fa1039",
+    "url": "/static/css/main.a158664c.chunk.css"
+  },
+  {
+    "revision": "ebd0d67609e24ff26ab255eb40cb68e5",
+    "url": "/index.html"
+  }
+];
