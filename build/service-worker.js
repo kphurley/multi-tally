fa191a1488c856b122f7603e2d1db4e062ee6a70
Kevin Hurley <kphurley@gmail.com>
2019-01-20 08:01:49 -0600

    Add build assets and update scripts

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4cd8b36e50f36ad8bde9035a6808fe3.js"
+  "/multi-tally/precache-manifest.821bd6628904e4f4118ac347cd641f00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/multi-tally/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
